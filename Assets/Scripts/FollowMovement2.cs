028237b504c9d7e715cb963df9803cb7d4ca441a
Jin-Ch-Cal <63160115+Jin-Ch-Cal@users.noreply.github.com>
2020-04-14 22:17:53 +0800

    SynMoveModelandBuilidng
    Synchronized the movenment of the Builidng and the Model. Rotation done. Scale done (haven't test). Position has bug (not proportional).

---
+++ Assets/Scripts/FollowMovement2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMovement2 : MonoBehaviour
+{
+    public GameObject master;
+
+    public Vector3 deltaPosition;
+
+
+    void Start()
+    {
+        deltaPosition = transform.position - master.transform.position;
+    }
+
+    void Update()
+    {
+        transform.rotation = master.transform.rotation;
+        transform.position = master.transform.position + deltaPosition;
+
+        transform.localScale = master.transform.localScale * 250;
+
+    }
+}
